1e4b4da80118befa5392c218643a291a99ece811
MoazHassan2022 <moaz25jan2015@gmail.com>
2023-12-06 21:18:49 +0200

    removed unnecessary lines

--- hnsw.py
+++ hnsw.py
@@ -24,26 +24,21 @@ class HNSW:
         cosine_similarity = dot_product / (norm_vec1 * norm_vec2)
         return cosine_similarity
 
-    # Find the nearest node in a layer
-    def _find_nearest(self, layer: Dict[int, Node], node: Node) -> Node:
-        nearest_node = max(layer.values(), key=lambda n: self.calculate_similarity(n, node))
-        return nearest_node
-
-    # This method creates a connection between two nodes by adding each node to the other's list of neighbours.
-    def _create_connection(self, node1: Node, node2: Node):
-        node1.neighbours.add(node2)
-
     def insert(self, node: Node):
         probabilities = [np.exp(-i / self.m_L) * (1 - np.exp(-1 / self.m_L)) for i in range(len(self.layers))]
         for i in range(len(self.layers)):  # start from the bottom layer
             self.layers[i][node.id] = Node(node.id, node.vector)
             if random.random() - sum(probabilities[:i]) < probabilities[i] :
                 break
+    
+    # This method creates a connection between two nodes by adding each node to the other's list of neighbours.
+    def _create_connection(self, node1: Node, node2: Node):
+        node1.neighbours.add(node2)
 
     def connect_nodes(self):
         for i,layer in enumerate(self.layers):
             nodes = list(layer.values())
-            with open(f'layers/layer_{i}', "w") as fout:
+            with open(f'layers/layer_{i}.csv', "w") as fout:
                 for node in layer.values():
                     # we need to sort the nodes by their distance to the current node
                     nodes.sort(key=lambda n: self.calculate_similarity(n, node), reverse=True)
@@ -55,7 +50,7 @@ class HNSW:
     def insert_records(self, rows: List[Dict[int, Annotated[List[float], 70]]]):
         # insert records to the file such that each file has self.records_per_file row of records and name the file as data_0, data_1, etc.
         for i in range(len(rows) // self.records_per_file):
-            with open(f'data/data_{i}', "w") as fout:
+            with open(f'data/data_{i}.csv', "w") as fout:
                 for row in rows[i*self.records_per_file:(i+1)*self.records_per_file]:
                     id, embed = row["id"], row["embed"]
                     row_str = f"{id}," + ",".join([str(e) for e in embed])
@@ -104,16 +99,14 @@ class HNSW:
         while len(random_lines) < min(num_nodes_down,self.layer_sizes[curr_layer]):
             random_lines.add(random.randint(0, self.layer_sizes[curr_layer] - 1))
         
-        print("random_lines len: ", len(random_lines))
-        
         # get the records of the entry points from the top layer file
-        curr_node_records = [lc.getline(f'layers/layer_{curr_layer}', random_line + 1) for random_line in random_lines]
+        curr_node_records = [lc.getline(f'layers/layer_{curr_layer}.csv', random_line + 1) for random_line in random_lines]
         
         # get the ids of the entry points
         curr_ids = [int(curr_node_record.split(',')[0]) for curr_node_record in curr_node_records]
         
         # get the records of the entry points from the file of the data
-        curr_data = [lc.getline(f'data/data_{curr_id // self.records_per_file}', curr_id % self.records_per_file + 1) for curr_id in curr_ids]
+        curr_data = [lc.getline(f'data/data_{curr_id // self.records_per_file}.csv', curr_id % self.records_per_file + 1) for curr_id in curr_ids]
         
         # convert to nodes
         curr_nodes = [Node(curr_node_id, [float(e) for e in curr_node_data.split(',')[1:]]) for curr_node_id, curr_node_data in zip(curr_ids, curr_data)]
@@ -132,7 +125,7 @@ class HNSW:
                 for n in record_ids:
                     n = int(n)
                     if not curr_neighbours_nodes.get(n):
-                        data_line_splitted = lc.getline(f'data/data_{n // self.records_per_file}', n % self.records_per_file + 1).split(',')
+                        data_line_splitted = lc.getline(f'data/data_{n // self.records_per_file}.csv', n % self.records_per_file + 1).split(',')
                         curr_neighbours_nodes[n] = Node(n, [float(e) for e in data_line_splitted[1:]])
                         
             # add current data to the neighbours set
@@ -162,11 +155,11 @@ class HNSW:
                     return max_similarity[0: top_n]
         
                 # get the records of the current nodes from the next layer
-                curr_node_records = [file_binary_search(f'layers/layer_{curr_layer}', curr_id, self.layer_sizes[curr_layer]) if curr_layer != 0 else lc.getline(f'layers/layer_0', curr_id + 1) for curr_id in curr_ids]
+                curr_node_records = [file_binary_search(f'layers/layer_{curr_layer}.csv', curr_id, self.layer_sizes[curr_layer]) if curr_layer != 0 else lc.getline(f'layers/layer_0', curr_id + 1) for curr_id in curr_ids]
                 continue
         
             # get the records of the nearest neighbours from the file of the data using binary search and linecache module
-            curr_node_records = [file_binary_search(f'layers/layer_{curr_layer}', max_id, self.layer_sizes[curr_layer]) for max_id in max_ids]
+            curr_node_records = [file_binary_search(f'layers/layer_{curr_layer}.csv', max_id, self.layer_sizes[curr_layer]) for max_id in max_ids]
             curr_ids = max_ids
             curr_nodes = [curr_neighbours_nodes[max_id] for max_id in max_ids]
     def retrive(self, query: Annotated[List[float], 70], top_k = 1):
@@ -174,11 +167,16 @@ class HNSW:
         # why self.M + 1? because in layer file, every node has itself + M neighbours
         # and sometimes we will have closed groups of such nodes, each group has M + 1 nodes
         top_nodes = self.retrive_n(query, min(self.M + 1, top_k))
-        if len(top_nodes) == top_k:
-            top_nodes_ids = [i.id for i in top_nodes]
+        top_nodes_ids = [i.id for i in top_nodes]
+        
+        if len(top_nodes_ids) == top_k:
             return top_nodes_ids
         
-        top_nodes_dict = {node.id: node for node in top_nodes}
+        # add random nodes
+        top_nodes_ids.extend([random.randint(0, self.layer_sizes[0] - 1) for _ in range(top_k - len(top_nodes_ids))])
+        
+        return top_nodes_ids
+        """top_nodes_dict = {node.id: node for node in top_nodes}
         
         previous_len = 0
         new_len = len(top_nodes_dict)
@@ -203,4 +201,4 @@ class HNSW:
             # add random nodes
             top_nodes_ids.extend([random.randint(0, self.layer_sizes[0] - 1) for _ in range(top_k - new_len)])
             
-        return top_nodes_ids
+        return top_nodes_ids"""
