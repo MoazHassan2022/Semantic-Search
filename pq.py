97afdc2f8bfd59dcaf4e226dc461442080ac832f
MoazHassan2022 <moaz25jan2015@gmail.com>
2023-12-10 20:42:42 +0200

    enhanced query distances calculations

--- pq.py
+++ pq.py
@@ -11,7 +11,10 @@ class PQ:
         self.num_subvectors = num_subvectors
         self.num_centroids = num_centroids
 
-
+    # Calculate the eculedian distance between two nodes
+    def eculedian_distance(self, node1: Node, node2: Node) -> float:
+        return np.linalg.norm(node1.pq_code - node2.pq_code)
+    
     # Calculate the distance between two nodes by calculating the cosine similarity between their vectors.
     def calculate_similarity(self, node1: Node, node2: Node) -> float:
         dot_product = np.dot(node1.pq_code, node2.pq_code)
@@ -31,7 +34,6 @@ class PQ:
             codebooks.append(kmeans.cluster_centers_)
             kmeans_models.append(kmeans)
         self.codebooks = codebooks
-        self.kmeans_models = kmeans_models
         self.data_size = len(rows)
 
         result_records = np.zeros((len(rows), self.num_subvectors), dtype=np.uint16)
@@ -69,8 +71,44 @@ class PQ:
         '''
         # Encode the query vector using Product Quantization
         sub_vectors = np.array_split(query[0], self.num_subvectors)
-        query = [kmeans.predict(subvector.reshape(1, -1))[0] for subvector, kmeans in zip(sub_vectors, self.kmeans_models)]
-        query_node = Node(-1, query)
+        
+        # Caclulate eculidean distance between all the query vector subvectors and each of the centroids of that subvector, and store it in distances matrix
+        # Every element of query_centroids_distances is a vector of size num_centroids, which contains the eculidean distance between the query vector subvector and each of the centroids of that subvector
+        query_centroids_distances = np.zeros((self.num_subvectors, self.num_centroids))
+        # codebooks is num_subvectors * num_centroids * ds, ds = 70 / num_subvectors
+        # sub_vectors is num_subvectors * ds
+        for i in range(self.num_subvectors):
+            query_centroids_distances[i] = np.linalg.norm(self.codebooks[i] - sub_vectors[i], axis=1)
+        
+        # Read all records from the files
+        # For loop over the files and get the top_k records from each file
+        records = np.zeros((self.data_size, self.num_subvectors), dtype=np.uint16)
+        records_counter = 0
+        for i in range(self.data_size // self.records_per_file):
+            with open(f'data/data_{i}', "r") as fin:
+                lines = fin.readlines()
+                records = []
+                for line in lines:
+                    splitted_line = line.split(",") # id, pq_code
+                    id, pq_code = int(splitted_line[0]), splitted_line[1:]
+                    pq_code = np.array([np.uint16(e) for e in pq_code])
+                    records[records_counter] = pq_code
+                    records_counter += 1
+                    
+        # Now, we have the distances between the query vector subvectors and each of the centroids of that subvector
+        # We need to find the distances between the query vector and each of the records in the database, based on ids of centroids in each record
+        query_records_distances = np.zeros((self.data_size))
+        for i in range(self.num_subvectors):
+            print(records[0][i])
+            query_records_distances += query_centroids_distances[i, records[:, i]]
+            
+        # Argsort the records based on the distances between the query vector and each of the records in the database
+        # Note that indices are the records ids
+        sorted_records_indices = np.argsort(query_records_distances)[:top_k]
+        
+        # Return the top_k records ids
+        return sorted_records_indices
+        """ query_node = Node(-1, query)
         # in for loop over the files and get the top_k records from each file
         all_top_k_records = []
         for i in range(self.data_size // self.records_per_file):
@@ -83,11 +121,11 @@ class PQ:
                     pq_code = np.array([np.uint16(e) for e in pq_code])
                     records.append(Node(id, pq_code))
                 # get the top_k records from the current file
-                sorted_records = sorted(records, key=lambda x: self.calculate_similarity(x, query_node), reverse=True)
+                sorted_records = sorted(records, key=lambda x: self.eculedian_distance(x, query_node), reverse=True)
                 top_k_records = sorted_records[:top_k]
                 # append the top_k_records to a list
                 all_top_k_records.extend(top_k_records)
         # sort the list based on cosine similarity
-        sorted_all_top_k_records = sorted(all_top_k_records, key=lambda x: self.calculate_similarity(x, query_node), reverse=True)
+        sorted_all_top_k_records = sorted(all_top_k_records, key=lambda x: self.eculedian_distance(x, query_node), reverse=True)
         # return the top_k records
-        return [record.id for record in sorted_all_top_k_records[:top_k]]
+        return [record.id for record in sorted_all_top_k_records[:top_k]] """
