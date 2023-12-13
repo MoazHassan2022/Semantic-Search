c5e4e4f83e72618c9efd5c3cf5f18a083e452224
mohamednabilabdelfattah <mohamed.nabi.5596@gmail.com>
2023-12-13 19:55:17 +0200

    implemented inverted_index

--- vec_db.py
+++ vec_db.py
@@ -1,7 +1,8 @@
 import numpy as np
 from typing import Dict, List, Annotated
 from sklearn.cluster import KMeans
-
+from collections import defaultdict
+from linecache import getline
 class VecDB:
 
     def __init__(self, num_subvectors = 14, file_path = None, new_db = True) -> None:
@@ -11,10 +12,21 @@ class VecDB:
             self.data_size = int(file_path)
         else:
             self.file_path = 'saved_db'
+        # Initialize the inverted index
+        self.inverted_index = defaultdict(list)
         if new_db == False:
             # Load the self.codebooks from the codebooks file
             self.codebooks_file_path = 'codebooks' + file_path # file_path maybe 1000000
+            self.inverted_index_path = 'inverted_index' + file_path
             self.load_codebooks()
+            self.load_index()
+    
+    def calculate_similarity(self, node1, node2) -> float:
+        dot_product = np.dot(node1, node2)
+        norm_vec1 = np.linalg.norm(node1)
+        norm_vec2 = np.linalg.norm(node2)
+        cosine_similarity = dot_product / (norm_vec1 * norm_vec2)
+        return cosine_similarity
 
     def select_parameters(self):
         if self.data_size <= 1000:
@@ -34,13 +46,32 @@ class VecDB:
             with open(self.codebooks_file_path, "r") as fin:
                 codebooks.append(np.loadtxt(fin, delimiter=",", dtype=np.float32, skiprows=i * self.num_centroids, max_rows=self.num_centroids))
         self.codebooks = codebooks
+
+    def load_index(self):
+        self.inverted_index = defaultdict(list)
+        sub_space,centroid_id = 0,0
+        with open(self.inverted_index_path, "r") as fin:
+            for line in fin:
+                self.inverted_index[(sub_space,centroid_id)] = [np.uint16(i) for i in line.split(',')]
+                centroid_id += 1
+                if(centroid_id == self.num_centroids):
+                    sub_space += 1
+                    centroid_id = 0
     
     def insert_records(self, rows: List[Dict[int, Annotated[List[float], 70]]]):
-        rows = np.array([row["embed"] for row in rows])
-        
+        if isinstance(rows, np.ndarray) and rows.shape[1] == 70:
+            convert_rows = False
+        else:
+            convert_rows = True
+        if convert_rows:
+            rows = np.array([row["embed"] for row in rows])
         self.data_size = len(rows)
         
         self.select_parameters()
+        
+        # save the data in file 
+        with open(self.file_path, "w") as fout:
+            np.savetxt(fout, rows, delimiter=",", fmt="%f")
         
         # Train only on 1 million, if data is more than 1 million, else, train on all data
         training_data = None
@@ -63,15 +94,22 @@ class VecDB:
         with open(f'codebooks', "w") as fout:
             for i in range(self.num_subvectors):
                 np.savetxt(fout, codebooks[i], delimiter=",", fmt="%.8f")
-        # write to only 1 file
-        with open(self.file_path, "w") as fout:
-            pq_codes = np.zeros((self.data_size, self.num_subvectors), dtype=np.uint16)
-            for i in range(self.num_subvectors):
-                # Predict the centroid of each subvector for each record
-                pq_codes[:, i] = kmeans_models[i].predict(rows[:, i * subvector_size : (i + 1) * subvector_size])
-            # Save the pq_codes array to the file
-            np.savetxt(fout, pq_codes, delimiter=",", fmt="%d")
 
+        pq_codes = np.zeros((self.data_size, self.num_subvectors), dtype=np.uint16)
+        for i in range(self.num_subvectors):
+            # Predict the centroid of each subvector for each record
+            pq_codes[:, i] = kmeans_models[i].predict(rows[:, i * subvector_size : (i + 1) * subvector_size])
+            
+        # Update the inverted index during insertion
+        for i in range(self.num_subvectors):
+            for j in range(self.num_centroids):
+                records_with_centroid_j = np.where(pq_codes[:, i] == j)[0]
+                self.inverted_index[(i, j)].extend(records_with_centroid_j.tolist())
+        # Save the inverted index to the inverted index file
+        with open(f'inverted_index', "w") as fout:
+            for key in self.inverted_index:
+                fout.write(','.join([str(id) for id in self.inverted_index[key]])+"\n")
+                
     def retrive(self, query: Annotated[List[float], 70], top_k = 1):
         query = query[0]
         subvector_size = 70 // self.num_subvectors
@@ -84,36 +122,20 @@ class VecDB:
         for i in range(self.num_subvectors):
             query_centroids_distances[i] = np.linalg.norm(self.codebooks[i] - query[i * subvector_size : (i + 1) * subvector_size], axis=1)
         
-        # Read all records from the files
-        # create top_k_records array of size (self.k) that contain tuples of (record_id, distance) and initialize the distance with max int
-        # for each file, read all records and calculate the distance between the query vector and each of the records in the database
-        # extract the top_k records from both prevoius top_k_records and the current file
-        # return the top_k records ids
-        top_k_records = np.zeros((top_k), dtype=[('record_id', np.int32), ('distance', np.float32)])
-        # Set distance to the maximum finite float32 value
-        top_k_records['distance'] = np.finfo(np.float32).max
+        # Use the inverted index to filter potential records
+        potential_records = set()
+        for k in range(self.num_subvectors):
+            indexes = np.argsort(query_centroids_distances[k])[:10]
+            for index in indexes:
+                potential_records.update(self.inverted_index[(k, index)])
         
-        for i in range(self.data_size // self.records_per_read):
-            # Open the database file
-            with open(self.file_path, 'r') as fin:
-                # Read the records from the file, paginated
-                records = np.loadtxt(fin, delimiter=",", dtype=np.uint16, skiprows=i * self.records_per_read, max_rows=self.records_per_read)
-                
-                # Now, we have the distances between the query vector subvectors and each of the centroids of that subvector
-                # We need to find the distances between the query vector and each of the records in the database, based on ids of centroids in each record
-                query_records_distances = np.zeros((self.records_per_read))
-                for k in range(self.num_subvectors):
-                    query_records_distances += query_centroids_distances[k, records[:, k]]
-                # merge distances with previous top_k_records in one array and sort them
-                distances = np.concatenate(( query_records_distances,top_k_records['distance']))
-                indices = np.argsort(distances)
-                curr_top_k_records = np.zeros((top_k), dtype=[('record_id', np.int32), ('distance', np.float32)])
-                for k in range(top_k):
-                    if indices[k] < self.records_per_read:
-                        curr_top_k_records[k] = (i * self.records_per_read + indices[k], distances[indices[k]])
-                    else: 
-                        curr_top_k_records[k] = top_k_records[indices[k] - self.records_per_read]
-                top_k_records = curr_top_k_records
-
+        records = np.zeros((len(potential_records), 71),dtype=np.float32)
+        # Read only the potential records from the files
+        for i,record_id in enumerate(potential_records):
+            # read the line and append it to records list with linecache module
+            record = getline(self.file_path, record_id + 1).split(',')
+            records[i] = np.array([record_id] + [np.float32(i) for i in record])
+        # sort the records based on cosine similarity between each record and query vector 
+        records = sorted(records, key=lambda x: self.calculate_similarity(x[1:], query), reverse=True)
         # Return the top_k records ids
-        return top_k_records['record_id']
+        return [np.int32(record[0]) for record in records[:top_k]]
